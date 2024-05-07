68a4a1d22452d90850e0164d2204f84a44536436
Viktoriia <nazaruk.v92@gmail.com>
2024-05-07 14:56:34 +0300

    fix copy

--- first-task.py
+++ first-task.py
@@ -2,7 +2,7 @@ import os
 import shutil
 import argparse
 
-def copy_files_recursively(source_dir, dest_dir):
+def copy_and_move_files(source_dir, dest_dir):
     if not os.path.exists(dest_dir):
         os.makedirs(dest_dir)
 
@@ -11,7 +11,7 @@ def copy_files_recursively(source_dir, dest_dir):
         dest_path = os.path.join(dest_dir, item)
 
         if os.path.isdir(source_path):
-            copy_files_recursively(source_path, dest_path)
+            copy_and_move_files(source_path, dest_path)
         else:
             _, extension = os.path.splitext(item)
             extension_dir = os.path.join(dest_dir, extension[1:])
@@ -20,12 +20,14 @@ def copy_files_recursively(source_dir, dest_dir):
             try:
                 shutil.copy2(source_path, extension_dir)
                 print(f"Копіювання {item} завершено")
+                shutil.move(source_path, dest_dir)
+                print(f"Переміщення {item} завершено")
             except Exception as e:
-                print(f"Помилка копіювання {item}: {e}")
+                print(f"Помилка копіювання або переміщення {item}: {e}")
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Копіювання файлів та сортування їх за розширенням')
+    parser = argparse.ArgumentParser(description='Копіювання файлів та переміщення їх у нову директорію')
     parser.add_argument('source_dir', type=str, help='Шлях до вихідної директорії')
     parser.add_argument('dest_dir', type=str, nargs='?', default='dist', help='Шлях до директорії призначення')
     args = parser.parse_args()
-    copy_files_recursively(args.source_dir, args.dest_dir)
+    copy_and_move_files(args.source_dir, args.dest_dir)
